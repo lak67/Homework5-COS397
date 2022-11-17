32bb36a4c5e4413e3652b03577fa47a1e6d9638c
donuto <77982082+donuto@users.noreply.github.com>
2022-11-16 19:20:36 -0500

    Create int_sort.py

---
+++ python_package_exercise/basic_sort_UNIQUE_SUFFIX/int_sort.py
@@ -0,0 +1,63 @@
+# =========================================================================
+#
+#  Copyright Ziv Yaniv
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# =========================================================================
+
+"""
+This module sorts lists of integers...
+"""
+
+
+def bubble(int_list):
+    n = len(int_list)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if int_list[j] > int_list[j + 1]:
+                swapped = True
+                int_list[j], int_list[j + 1] = int_list[j + 1], int_list[j]
+         
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
+    print("bubble sort")
+    for i in range(len(int_list)):
+        print("% d" % int_list[i], end=" ")
+
+
+def quick(int_list):
+    """
+    qsort docstring
+    """
+    print("quick sort")
+
+
+def insertion(int_list):
+    """
+    insertion docstring
+    """
+    print("insertion sort")
