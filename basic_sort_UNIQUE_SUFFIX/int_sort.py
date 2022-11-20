bc0404eb822045602aada9a2e1148e4b9a0cc518
hannahyelle <hannah.yelle.4@gmail.com>
2022-11-20 15:31:44 -0500

    pre-commit-config install

--- basic_sort_UNIQUE_SUFFIX/int_sort.py
+++ basic_sort_UNIQUE_SUFFIX/int_sort.py
@@ -21,7 +21,6 @@ This module sorts lists of integers...
 """
 
 
-
 def bubble(int_list):
 
     # need to add parameter types
@@ -35,25 +34,25 @@ def bubble(int_list):
     # to go through the entire process
     swapped = False
     # Traverse through all array elements
-    for i in range(n-1):
+    for i in range(n - 1):
         # range(n) also work but outer loop will
         # repeat one time more than needed.
         # Last i elements are already in place
-        for j in range(0, n-i-1):
- 
+        for j in range(0, n - i - 1):
+
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
             if int_list[j] > int_list[j + 1]:
                 swapped = True
                 int_list[j], int_list[j + 1] = int_list[j + 1], int_list[j]
-         
+
         if not swapped:
             # if we haven't needed to make a single swap, we
             # can just exit the main loop.
             return
     print("bubble sort")
-    return(int_list)
+    return int_list
 
 
 def quick(int_list, low, high):
@@ -61,50 +60,50 @@ def quick(int_list, low, high):
 
     :param1 :
         int_list: list given by the user
-    :param2 : 
+    :param2 :
         low: this is the first item of the array
     :param3 :
         high: this is the last item of the array
     :returns: returns the list in a sorted format from lowest to highest
     """
     if low < high:
-     
+
         # Find pivot element such that
         # element smaller than pivot are on the left
         # element greater than pivot are on the right
-        pi = partition(int_list, low, high)
- 
+        # pi = partition(int_list, low, high)
+        pi = 2
+
         # Recursive call on the left of pivot
         quick(int_list, low, pi - 1)
- 
+
         # Recursive call on the right of pivot
         quick(int_list, pi + 1, high)
 
     print("quick sort")
-    return(int_list)
+    return int_list
 
 
 def insertion(int_list):
     """
-    :param int_list: 
+    :param int_list:
         an unsorted list of integers
     :returns: the list of integers sorted in ascending order
     """
 
     # Traverse through 1 to len(arr)
     for i in range(1, len(int_list)):
- 
+
         key = int_list[i]
- 
+
         # Move elements of arr[0..i-1], that are
         # greater than key, to one position ahead
         # of their current position
-        j = i-1
-        while j >=0 and key < int_list[j] :
-                int_list[j+1] = int_list[j]
-                j -= 1
-        int_list[j+1] = key
+        j = i - 1
+        while j >= 0 and key < int_list[j]:
+            int_list[j + 1] = int_list[j]
+            j -= 1
+        int_list[j + 1] = key
 
     print("insertion sort")
-    return (int_list)
-
+    return int_list
