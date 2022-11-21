d3da55f7e3459dddcffef10955f57348ed908df6
hannahyelle <hannah.yelle.4@gmail.com>
2022-11-20 23:19:09 -0500

    change import name for basic_sort_shell_software

--- test/test_basic_sort.py
+++ test/test_basic_sort.py
@@ -18,7 +18,7 @@
 
 import pytest
 import numpy as np
-from basic_sort_UNIQUE_SUFFIX.int_sort import bubble
+from basic_sort_shell_software.int_sort import bubble
 
 
 def is_sorted(self, int_list):
